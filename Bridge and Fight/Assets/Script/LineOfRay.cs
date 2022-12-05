b3504ee1bd722891a94d42f2a28e207404bea7bc
firzamc01 <102782455+firzamhmd@users.noreply.github.com>
2022-12-05 10:45:16 +0700

    rapikan script

--- Bridge and Fight/Assets/Script/LineOfRay.cs
+++ Bridge and Fight/Assets/Script/LineOfRay.cs
@@ -22,31 +22,40 @@ public class LineOfRay : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        RaycastHit2D hit = Physics2D.Linecast(lineTargets[0].transform.position, lineTargets[1].transform.position,layerMask);
-        if(hit.collider != null)
+        if (!PlayerDestroy.playerDestroy.isGameOver) 
         {
-            if(hit.collider.gameObject.tag == "obstacle")
+            lineConnector();
+        }
+       
+    }
+
+    void lineConnector() 
+    {
+        RaycastHit2D hit = Physics2D.Linecast(lineTargets[0].transform.position, lineTargets[1].transform.position, layerMask);
+        if (hit.collider != null)
+        {
+            if (hit.collider.gameObject.tag == "obstacle")
             {
                 //Debug.DrawLine(transform.position, target.position, Color.red);
                 Debug.DrawLine(lineTargets[0].transform.position, lineTargets[1].position, Color.red);
                 isTouchObstcale = true;
             }
-            else if(hit.collider.gameObject.tag == "Medium Enemy" && BulletConnect.bulletConnect.isConnected)
+            else if (hit.collider.gameObject.tag == "Medium Enemy" && BulletConnect.bulletConnect.isConnected)
             {
                 //Debug.DrawLine(transform.position, target.position, Color.blue);
                 Debug.DrawLine(lineTargets[0].transform.position, lineTargets[1].position, Color.blue);
                 estat = hit.collider.gameObject.GetComponent<EnemyStat>();
-                estat.health -= 10*Time.deltaTime;
+                estat.health -= 10 * Time.deltaTime;
                 //Instantiate(laserParticle, hit.collider.transform.position, Quaternion.identity);
             }
-            else if(hit.collider.gameObject.tag=="Normal Enemy" && BulletConnect.bulletConnect.isConnected) 
+            else if (hit.collider.gameObject.tag == "Normal Enemy" && BulletConnect.bulletConnect.isConnected)
             {
                 estat = hit.collider.gameObject.GetComponent<EnemyStat>();
                 estat.health -= 10;
             }
             else
             {
-               //Debug.DrawLine(transform.position, target.position, Color.green);
+                //Debug.DrawLine(transform.position, target.position, Color.green);
                 Debug.DrawLine(lineTargets[0].transform.position, lineTargets[1].position, Color.green);
                 isTouchObstcale = false;
             }
