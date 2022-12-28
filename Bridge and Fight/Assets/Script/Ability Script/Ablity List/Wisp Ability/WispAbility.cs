15c100dec61a99a1fb47d1fc7b4de5c476713745
firzamc01 <102782455+firzamhmd@users.noreply.github.com>
2022-12-28 22:21:36 +0700

    proses ability ke 4

--- Bridge and Fight/Assets/Script/Ability Script/Ablity List/Wisp Ability/WispAbility.cs
+++ Bridge and Fight/Assets/Script/Ability Script/Ablity List/Wisp Ability/WispAbility.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FireAbility : MonoBehaviour
+public class WispAbility : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
